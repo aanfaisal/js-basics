81c263e7c2436ca4dca9246a22ca81085042c574
aanfaisal <aan.faisalal@gmail.com>
2018-12-31 17:38:49 +0700

    Basic : 1. Variabel

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
- // this is my first js 
- console.log("Hello World !");
+// this is my first js 
+//  console.log("Hello World !");
+
+// variabel
+let name = "Aan";
+console.log(name);
+
+// Rule Penamaan Variabel => 1. Jangan memakai reserved keyword 
+// 2. Harus bermakna => 3. Jangan make angka di depan variabel cth : (1name)
+// 4. Jangan ada spasi dan tanda strip(-) => 5. Variabel itu case-sensitive
