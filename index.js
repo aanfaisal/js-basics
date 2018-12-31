d24b556b7c4c8ead01b344f3af96369a3984bfaa
aanfaisal <aan.faisalal@gmail.com>
2018-12-31 19:11:07 +0700

    Basic : 3. Primitive Types

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-// this is my first js 
-//  console.log("Hello World !");
+// // this is my first js 
+// //  console.log("Hello World !");
 
-// variabel
-let name = "Aan";
-console.log(name);
+// // variabel
+// let name = "Aan";
+// console.log(name);
 
-// Rule Penamaan Variabel => 1. Jangan memakai reserved keyword 
-// 2. Harus bermakna => 3. Jangan make angka di depan variabel cth : (1name)
-// 4. Jangan ada spasi dan tanda strip(-) => 5. Variabel itu case-sensitive
+// // Rule Penamaan Variabel => 1. Jangan memakai reserved keyword 
+// // 2. Harus bermakna => 3. Jangan make angka di depan variabel cth : (1name)
+// // 4. Jangan ada spasi dan tanda strip(-) => 5. Variabel itu case-sensitive
 
-// konstanta
-const interestRate = 0.3;
-//interestRate = 1;
-console.log(interestRate);
+// // konstanta
+// const interestRate = 0.3;
+// //interestRate = 1;
+// console.log(interestRate);
+
+//  Primitive Types
+let name = "Aan"; // ini string
+let age = 20; // ini number
+let isApproved = true; // ini boolean -- true / false
+let firstName = undefined;
+let lastName = null;
+
+
+
+
 
