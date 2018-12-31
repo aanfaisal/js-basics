b9ba213a0a297580b385283771f998abe1e51768
aanfaisal <aan.faisalal@gmail.com>
2018-12-31 21:48:31 +0700

    Base : 7. Function

--- index.js
+++ index.js
@@ -35,10 +35,18 @@
 
 // console.log(person.name);
 
-// Array digunakan untuk menyimpan list sesuatu :o
+// // Array digunakan untuk menyimpan list sesuatu :o
 
-let selectedColor = ["red", "blue"];
+// let selectedColor = ["red", "blue"];
 
-selectedColor[2]= 1;
+// selectedColor[2]= 1;
 
-console.log(selectedColor.length);
+// console.log(selectedColor.length);
+
+// Function ==> greet(name) name disini adalah PARAMETER
+function greet(name, LastName) {
+  console.log('Hello ' + name + LastName)
+}
+
+// Aan disini adalah ARGUMENT
+greet('Aan', 'Faisal');
