ee7b00abd735d6638708421f1cf0027045753668
aanfaisal <aan.faisalal@gmail.com>
2018-12-31 19:05:00 +0700

    Basic : 2. Konstanta

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ console.log(name);
 // Rule Penamaan Variabel => 1. Jangan memakai reserved keyword 
 // 2. Harus bermakna => 3. Jangan make angka di depan variabel cth : (1name)
 // 4. Jangan ada spasi dan tanda strip(-) => 5. Variabel itu case-sensitive
+
+// konstanta
+const interestRate = 0.3;
+//interestRate = 1;
+console.log(interestRate);
+
