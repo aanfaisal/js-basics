6ab717945f5fe88561bafde326b1edbd353410ba
aanfaisal <aan.faisalal@gmail.com>
2018-12-31 22:46:32 +0700

    Operator : 4. Comparison Operator

--- operators.js
+++ operators.js
@@ -1,25 +1,43 @@
-// Operator Aritmatika
-let x = 10;
-let y = 3;
+// // Operator Aritmatika
+// let x = 10;
+// let y = 3;
 
-console.log( x + y);
-console.log( x - y);
-console.log( x * y);
-console.log( x / y);
-console.log( x % y);
-console.log( x ** y);
+// console.log( x + y);
+// console.log( x - y);
+// console.log( x * y);
+// console.log( x / y);
+// console.log( x % y);
+// console.log( x ** y);
 
-// Increment (++)
-console.log( x++);
-console.log( x );
+// // Increment (++)
+// console.log( x++);
+// console.log( x );
 
-// Decrement (--)
-console.log( --x );
+// // Decrement (--)
+// console.log( --x );
 
+// // Assignment Operator alias ( = ) / sama dengan
 
+// let x = 10;
 
+// x = x + 5; //sama dengan 
+// x += 5;
 
+// x = x * 3; //sama dengan
+// x *= 3;
 
+// console.log(x);
+
+let x = 1;
+// Relasional Operator
+console.log( x > 0);
+console.log( x >= 0);
+console.log( x < 0);
+console.log( x <= 0);
+
+// Equality Operator
+console.log( x === 0);
+console.log( x !== 0);
 
 
 
