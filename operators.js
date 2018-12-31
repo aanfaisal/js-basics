48d0993fea16240c50b897c69eebb8e721776c44
aanfaisal <aan.faisalal@gmail.com>
2018-12-31 22:06:24 +0700

    Operators : 2. Aritmatika

---
+++ operators.js
@@ -0,0 +1,27 @@
+// Operator Aritmatika
+let x = 10;
+let y = 3;
+
+console.log( x + y);
+console.log( x - y);
+console.log( x * y);
+console.log( x / y);
+console.log( x % y);
+console.log( x ** y);
+
+// Increment (++)
+console.log( x++);
+console.log( x );
+
+// Decrement (--)
+console.log( --x );
+
+
+
+
+
+
+
+
+
+
